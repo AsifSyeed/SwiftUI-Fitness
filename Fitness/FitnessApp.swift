c0650aed1693325ddd0dfc9b119833610f68bc76
AsifSyeed <asifsyeed666@gmail.com>
2022-10-20 17:02:23 +0600

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,20 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by BS901 on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
