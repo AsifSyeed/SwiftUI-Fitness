0b59ea269ea8733ba4e78c7d807309a048820c9c
AsifSyeed <asifsyeed666@gmail.com>
2022-10-23 21:25:44 +0600

    Pod initialization and firebase configure

--- Fitness/FitnessApp.swift
+++ Fitness/FitnessApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FitnessApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
@@ -16,5 +20,14 @@ struct FitnessApp: App {
             LandingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
     }
 }
