81127b8b400e46befdf68af8c50f3e7edce2e7c8
AsifSyeed <asifsyeed666@gmail.com>
2022-10-25 15:28:20 +0600

    Create challenges in firebase

---
+++ Fitness/Models/Challenge.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge.swift
+//  Fitness
+//
+//  Created by BS901 on 10/25/22.
+//
+
+import Foundation
+
+struct Challenge: Codable {
+    let exercise: String
+    let startAmount: Int
+    let increase: Int
+    let length: Int
+    let userId: String
+    let startDate: Date
+}
